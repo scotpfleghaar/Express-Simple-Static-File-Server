dcf64b17d38cf5b6357a1e831964f79f5c5bcfcb
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-12-07 12:26:01 -0600

    added morgan

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const morgan = require("morgan");
 
 var app = express();
+app.use(morgan("short"));
 
-app.use(function (req, res, next) {
-    console.log("Request IP: " + req.url);
-    console.log("Request Date: " + new Date());
-    next();
+var staticPath = path.join(__dirname, "static");
+app.use(express.static(staticPath));
+
+app.use(function (err, req, res, next) {
+    console.error(err);
+    next(err);
 });
 
 app.use(function (req, res, next) {
-    var filePath = path.join(__dirname, "static", req.url);
-    fs.stat(filePath, function (err, fileInfo) {
-        if (err) {
-            next();
-            return;
-        }
-        if (fileInfo.isFile()) {
-            res.sendFile(filePath);
-        } else {
-            next();
-        }
-    });
+    res.status(500);
+    res.send("Internal server error.");
 });
-
 
 app.use(function (req, res) {
     res.status(404);
     res.send("File not found");
 });
 
-
-
 app.listen(3000, function () {
     console.log("App is running on port 3000");
-})
+});
